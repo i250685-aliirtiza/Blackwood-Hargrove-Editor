157597596eb997ed5d07a6f9ddf3fe31cb66f6f0
i250685-aliirtiza <i250685@isb.nu.edu.pk>
2026-03-30 15:55:59 +0500

    cleaned folders

--- THE ARACANUM EDITOR/THE ARACANUM EDITOR.cpp
+++ THE ARACANUM EDITOR/THE ARACANUM EDITOR.cpp
@@ -751,6 +751,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
+
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
     //debugging helpers
